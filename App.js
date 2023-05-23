827bd32df4abb02843e8fb1e0af0eda3a1082635
Amber Hasan <amberhasan@Ambers-MacBook-Pro-2.local>
2023-05-23 11:59:03 -0500

    Basic counter

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Here is the counter", counter);
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Counter"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Button
+      </Button>
+      <Text>Here is the counter {counter}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
