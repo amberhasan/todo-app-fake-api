d7ef77f22839014ef64d22ea313533f7d9f3707b
Amber Hasan <amberhasan@Ambers-MacBook-Pro-2.local>
2023-05-24 11:18:09 -0500

    useEffect types

--- App.js
+++ App.js
@@ -4,10 +4,36 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [click, setClick] = useState(false);
+
+  // useEffect(() => {
+  //   console.log("Here is the counter", counter);
+  // });
 
   useEffect(() => {
-    console.log("Here is the counter", counter);
+    // this is componentDidUpdate()
+    console.log("When state updates I will get called");
+    // return () => {
+    //   console.log(
+    //     "I will get update before the state update component will update"
+    //   );
+    // };
   });
+
+  useEffect(() => {
+    // this is componentDidMount()
+    console.log("I am component Did Mount, and I only get called once");
+    // return () => {
+    //   console.log("I am unmounting");
+    // };
+  }, []);
+
+  useEffect(() => {
+    console.log("I get called when click changed", click);
+  }, [click]);
+
+  console.log("----------------------------------------");
+
   return (
     <View style={styles.container}>
       <Button
@@ -15,10 +41,17 @@ export default function App() {
         onPress={() => {
           setCounter(counter + 1);
         }}
-      >
-        Button
-      </Button>
+      />
+
       <Text>Here is the counter {counter}</Text>
+
+      <Button
+        title="Click"
+        onPress={() => {
+          setClick(!click);
+        }}
+      ></Button>
+      <Text>Click click click {click}</Text>
       <StatusBar style="auto" />
     </View>
   );
